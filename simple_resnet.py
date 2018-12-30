39d00206d4f71752d57e2fbf5dbb0a3c333c4db2
Ziteng Gao <gzt@outlook.com>
2018-12-30 21:40:13 +0800

    #5 commit.

--- simple_resnet.py
+++ simple_resnet.py
@@ -22,11 +22,11 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         medium = max(inplanes, planes)//2
         self.conv1 = conv1x1(inplanes, medium)
-        self.bn1 = nn.InstanceNorm2d(medium)
+        self.bn1 = nn.BatchNorm2d(medium)
         self.conv2 = conv3x3(medium, medium, stride=stride)
-        self.bn2 = nn.InstanceNorm2d(medium)
+        self.bn2 = nn.BatchNorm2d(medium)
         self.conv3 = conv1x1(medium, planes)
-        self.bn3 = nn.InstanceNorm2d(planes)
+        self.bn3 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
         if stride == 2 or inplanes != planes:
             self.transition = conv3x3(inplanes, planes, stride=stride)
